b241a5ed8fca1361eaf9fb442b0e94141b99fe65
Teo <acuatoria@gmail.com>
2022-08-26 16:16:06 +0200

    svg

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,9 +1,16 @@
 <template>
   <div inline-flex text-2xl font-300 cursor-default>
     <div flex flex-col children:mx-auto>
-      <img w-18 h-18 inline-block src="/src/assets/logo_256_simple.svg">
-      <span text-green5 mt--2>Nuxt 3</span>
+      <div w-18 h-18 class="logo" />
+      <span text-purple9 mt--2>Linkin</span>
     </div>
   </div>
 </template>
 
+<style scoped>
+.logo{
+  background-image: url('src/assets/logo_256_simple.svg');
+  background-size: cover;
+  border:0px !important;
+}
+</style>
