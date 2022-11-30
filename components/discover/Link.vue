57d93b01ea47e7f8aba83cc49dbfb8c5a645f100
Teo <acuatoria@gmail.com>
2022-12-01 00:11:15 +0100

    fix link title

--- components/discover/Link.vue
+++ components/discover/Link.vue
@@ -22,7 +22,7 @@ export default {
           <a :href="record.url" target="_blank">
             <div class="item-text">
               <span text-size-lg>
-                {{ reocord.url_title || record.title || '&nbsp;' }}
+                {{ record.url_title || record.title || '&nbsp;' }}
               </span>
             </div>
 
