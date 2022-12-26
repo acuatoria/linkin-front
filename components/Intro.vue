6ce548b20fab21cbab5b5e7e59233bffe15623ba
Teo <acuatoria@gmail.com>
2022-12-26 11:19:44 +0100

    a little bit of seo

--- components/Intro.vue
+++ components/Intro.vue
@@ -14,17 +14,19 @@ const items = [
     max-width="600"
     tile
   >
-    <div text-lg mb-3>
-      Here you can do the following with your bookmarks:
+    <div text-lg m-3>
+      <h1>Manage your bookmarks, discover links, share links and comment them</h1>
     </div>
     <v-list-item
       v-for="(item, i) in items"
       :key="i"
     >
       <div class="flex" mb-3>
-        <span text-left text-2xl>
-          <v-icon v-if="item.icon" :color="color_return(i + 1)" :icon="item.icon" />
-          <span ml-3>{{ item.text }}</span>
+        <span text-left>
+          <h2 text-2xl>
+            <v-icon v-if="item.icon" :color="color_return(i + 1)" :icon="item.icon" />
+            <span ml-3>{{ item.text }}</span>
+          </h2>
           <br><span text-lg text-gray>{{ item.subtext }}</span>
         </span>
       </div>
@@ -39,4 +41,13 @@ const items = [
       </v-btn>
     </NuxtLink>
   </div>
+  <span text-gray text-sm>
+    Search is important, but also discover new things.
+  </span>
 </template>
+
+<style scoped>
+h2{
+  font-size: ;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <script setup>
 useHead({
-  title: 'Home',
+  title: 'Pyxilink, where links want to be',
+  meta: [{
+    name: 'description',
+    content: 'Here you can manage your bookmarks, share your links as collections, comment on urls and discover interesting things',
+  }],
 })
 const route = useRoute()
 const token = route.query.token
